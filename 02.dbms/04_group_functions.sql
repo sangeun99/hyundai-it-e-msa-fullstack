4a2c2e163e882069a3c347376337de8f84a2e88b
tori <sangeun99@khu.ac.kr>
2023-11-03 09:04:36 +0900

    📝 DBMS Chapter 04 그룹함수 (문제 풀기)

--- 02.dbms/04_group_functions.sql
+++ 02.dbms/04_group_functions.sql
@@ -33,3 +33,26 @@ WHERE   job NOT LIKE '%사원%'
 GROUP   BY job
 HAVING  SUM(sal) >= 1000
 ORDER   BY SUM(sal);
+
+-- 각 job별, 업무별 급여의 합계
+
+SELECT  job
+        ,sum(decode(deptno, '10', sal)) as d10
+        ,sum(decode(deptno, '20', sal)) as d20
+        ,sum(decode(deptno, '30', sal)) as d30
+        ,sum(sal)
+FROM    emp
+GROUP   BY job;
+
+-- CONN ace
+
+SELECT  job_id
+        ,sum(decode(department_id, '10', salary)) as d10        
+        ,sum(decode(department_id, '20', salary)) as d20
+        ,sum(decode(department_id, '50', salary)) as d50
+        ,sum(decode(department_id, '60', salary)) as d60
+        ,sum(decode(department_id, '90', salary)) as d90
+        ,sum(decode(department_id, '110', salary)) as d110
+        ,sum(salary)
+FROM    employees
+GROUP   BY job_id;
